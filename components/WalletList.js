62cadc1b4c61f966f1a1dece06073d4b923fb222
janoschwalter <janowalter@tsn.at>
2026-04-17 09:50:55 +0200

    Angefangen mit der Implementierung der WalletList klasse

--- components/WalletList.js
+++ components/WalletList.js
@@ -1,10 +1,28 @@
 app.component('wallet-list', {
     props: {
+        purchases: {
+            type: Array,
+            required: true
+        }
     },
     template:
     /*html*/
         `
+    <div class="wallet-list">
+        <h3>My Purchases</h3>
+        <ul>
+            <li v-for="purchase in purchases" :key="purchase.id">
+                <strong>{{ purchase.currency }}</strong> &mdash;
+                Amount: {{ purchase.amount }} &mdash;
+                Price: {{ purchase.price }} &mdash;
+                Date: {{ purchase.date }}
+                <button @click="$emit('delete-purchase', purchase.id)">Delete</button>
+            </li>
+        </ul>
+        <p v-if="purchases.length === 0">No purchases yet.</p>
+    </div>
     `,
+    emits: ['delete-purchase'],
     methods: {
 
     },
